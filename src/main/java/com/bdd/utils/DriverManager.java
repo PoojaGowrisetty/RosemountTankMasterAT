31a3608cd2958a7ab39d334c85d6b390f6661b14
Pooja Gowrisetty <pooja.gowrisetty@gmail.com>
2025-07-23 18:54:12 +0200

    updated

--- src/main/java/com/bdd/utils/DriverManager.java
+++ src/main/java/com/bdd/utils/DriverManager.java
@@ -18,29 +18,25 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 		            switch (browser.toLowerCase()) {
 		                case "chrome":
 		                    WebDriverManager.chromedriver().setup();
-		                    driver = new ChromeDriver();
-		                    driver.manage().window().maximize();
+		                    driver = new ChromeDriver();		                   
 		                    break;
 		                case "firefox":
-		                	//String GeckoDriverPath = "C:\\Users\\Shreyasu\\Pooja\\WebAutomation\\RosemountTankMasterAT\\drivers\\geckodriver.exe";
 		                	String GeckoDriverPath = currentDir + File.separator + "drivers" + File.separator + "geckodriver.exe";
 			                System.setProperty("webdriver.gecko.driver", GeckoDriverPath);
 		                    //WebDriverManager.firefoxdriver().setup();
 		                    driver = new FirefoxDriver();
-		                    driver.manage().window().maximize();
 		                    break;
 		                case "edge":
 		                    //WebDriverManager.edgedriver().setup();
 		                    String edgeDriverPath = currentDir + File.separator + "drivers" + File.separator + "msedgedriver.exe";
-		                	//String edgeDriverPath = "C:\\Users\\Shreyasu\\Pooja\\WebAutomation\\RosemountTankMasterAT\\drivers\\msedgedriver.exe"; 
 			                System.setProperty("webdriver.edge.driver", edgeDriverPath);
 			                driver = new EdgeDriver();
-		                    driver.manage().window().maximize();
 		                    break;
 		                default:
 		                    throw new IllegalArgumentException("Browser not supported: " + browser);
 		            }
 		        }
+		        driver.manage().window().maximize();
 		        return driver;
 		    }
 		 
